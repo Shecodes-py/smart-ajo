cf55a3714cd6e42af81e7574ff3e25484ce6efba
Ella Udotong <94646178+Shecodes-py@users.noreply.github.com>
2026-05-09 18:02:22 +0100

    Cnfigured Database

--- smartajo/settings.py
+++ smartajo/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from datetime import timedelta
 from dotenv import load_dotenv
+import dj_database_url
 
 load_dotenv()  
 
@@ -91,14 +92,11 @@ WSGI_APPLICATION = 'smartajo.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
